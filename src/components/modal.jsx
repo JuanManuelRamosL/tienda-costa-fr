07c802f181223f306e0ba3b6b80acd03fa9081f2
EzeeRL <ezequielramoslencina@gmail.com>
2024-12-07 00:16:39 -0300

    modal iniciar sesion

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,6 +1,10 @@
+import Link from "next/link";
 import React from "react";
+import useStore from "../../store";
+import styles from "./modal.module.css"
 
 const StockModal = ({ show, onClose }) => {
+  const user = useStore((state) => state.user);
   if (!show) return null; // No renderizar el modal si no debe mostrarse
 
   return (
@@ -8,7 +12,9 @@ const StockModal = ({ show, onClose }) => {
       className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50"
       role="dialog"
     >
-      <div className="bg-white rounded-lg shadow-lg p-6 w-80 text-center">
+      {
+        user ? (
+        <div className="bg-white rounded-lg shadow-lg p-6 w-80 text-center">
         <h2 className="text-xl font-bold mb-4 text-red-600">
           ¡Stock insuficiente!
         </h2>
@@ -22,6 +28,28 @@ const StockModal = ({ show, onClose }) => {
           Cerrar
         </button>
       </div>
+        ):(
+          <div className={styles.modalOverlay}>
+          <div className={styles.modalContainer}>
+            <h2 className={styles.modalTitle}>
+              ¡Debes iniciar sesión para continuar!
+            </h2>
+            <p className={styles.modalMessage}>
+              Por favor, inicia sesión para completar esta compra.
+            </p>
+            <div className={styles.modalButtons}>
+              <Link href="/auth/login" className={styles.modalLogin}>
+                Iniciar Sesión
+              </Link>
+              <button onClick={onClose} className={styles.modalClose}>
+                Cerrar
+              </button>
+            </div>
+          </div>
+        </div>
+        
+        )}
+      
     </div>
   );
 };
